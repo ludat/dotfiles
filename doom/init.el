6007604b0c8abdaa3d48ee73fa5757e0d6cfcb99
Lucas David Traverso <lucas6246@gmail.com>
2023-12-09 02:28:09 -0300

    Use lsp for elixir

--- doom/init.el
+++ doom/init.el
@@ -125,7 +125,7 @@
        data              ; config/data formats
        ;;dart   ; paint ui and not much else
        ;;dhall
-       elixir            ; erlang done right
+       (elixir +lsp +tree-sitter)            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
