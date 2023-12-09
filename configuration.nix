14d9618b216443e308e041cc36096ff15b973f78
Lucas David Traverso <lucas6246@gmail.com>
2023-12-09 02:27:59 -0300

    Allow some ports so that syncthing works again

--- configuration.nix
+++ configuration.nix
@@ -278,6 +278,8 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
