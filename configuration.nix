ec2d8a54f60d94d72a0cc22a0e6bd75ec1ff6fb7
Lucas David Traverso <lucas6246@gmail.com>
2024-03-17 18:42:35 -0300

    Allow ipv6 localhost for unbound

--- configuration.nix
+++ configuration.nix
@@ -44,7 +44,7 @@ in {
     resolveLocalQueries = true;
     settings = {
       server = {
-        interface = [ "127.0.0.1" ];
+        interface = [ "127.0.0.1" "::1" ];
       };
       forward-zone = [
         {
