b31379baf72123d926deb70675035af622ec41a4
Lucas David Traverso <lucas6246@gmail.com>
2025-12-31 03:19:50 -0300

    Add neogit

--- nvim/init.lua
+++ nvim/init.lua
@@ -149,6 +149,24 @@ require("lazy").setup({
     lazy = false,
     opts = {style = "warmer"},
   },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- -- Only one of these is needed.
+      -- "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+      -- "nvim-mini/mini.pick",           -- optional
+      -- "folke/snacks.nvim",             -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  },
 })
 
 -- Use Enter as colon
